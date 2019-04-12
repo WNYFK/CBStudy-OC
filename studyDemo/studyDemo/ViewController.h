94657847d122c38e54255ed87fa7f6c5afd25ac4
陈斌 <chenbin05@maoyan.com>
2019-04-12 19:25:17 +0800

    初始化

---
+++ studyDemo/studyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studyDemo
+//
+//  Created by ChenBin on 2019/4/12.
+//  Copyright © 2019 ChenBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ studyDemo/studyDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  studyDemo
+//
+//  Created by ChenBin on 2019/4/12.
+//  Copyright © 2019 ChenBin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
